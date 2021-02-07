202769b6b372f0c0f065b78636fc588c4fa89230
PersonBelowRocks <ulrikwithc@gmail.com>
2021-02-07 02:37:16 +0100

    add cool (and broken) explosion effect to  wand

--- src/com/PersonBelowRocks/myplugin/events/WandFunctionality.java
+++ src/com/PersonBelowRocks/myplugin/events/WandFunctionality.java
@@ -3,10 +3,7 @@ package com.PersonBelowRocks.myplugin.events;
 import com.PersonBelowRocks.myplugin.items.ItemManager;
 import com.PersonBelowRocks.myplugin.util.Utils;
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
-import org.bukkit.Location;
-import org.bukkit.Particle;
-import org.bukkit.Sound;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -16,6 +13,7 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.util.Vector;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Random;
 
@@ -41,37 +39,50 @@ public class WandFunctionality implements Listener {
         if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
             if (event.getItem() != null) {
                 if (event.getItem().getItemMeta().equals(ItemManager.wand.getItemMeta())) {
+                    long then = System.currentTimeMillis();
 
+                    boolean broken = false;
+
+                    double scalar = 0.5;
+                    double radius = (scalar + (scalarIncrement*maxIterations))*maxIterations;
                     // todo: improve performance
 
-                    Location playerLoc = invoker.getLocation();
+                    Location invokerLocation = invoker.getLocation();
+                    Location blockLookingAt = invoker.getTargetBlock(null, (int) Math.round(radius)).getLocation();
                     World world = invoker.getWorld();
 
-                    double pitch = (playerLoc.getPitch() + 90) * RADIANS_CONVERSION_FACTOR;
-                    double yaw  = (playerLoc.getYaw() + 90)  * RADIANS_CONVERSION_FACTOR;
+                    double pitch = (invokerLocation.getPitch() + 90) * RADIANS_CONVERSION_FACTOR;
+                    double yaw  = (invokerLocation.getYaw() + 90)  * RADIANS_CONVERSION_FACTOR;
 
                     double x = Math.sin(pitch) * Math.cos(yaw);
                     double y = Math.sin(pitch) * Math.sin(yaw);
                     double z = Math.cos(pitch);
 
-                    Vector3D invokerDirectionFacingVector = new Vector3D(x, z, y);
-                    Vector3D invokerLocationVector = new Vector3D(playerLoc.getX(), playerLoc.getY(), playerLoc.getZ());
+                    final Vector3D invokerDirectionFacingVector = new Vector3D(x, z, y);
+                    Vector3D invokerLocationVector = new Vector3D(invokerLocation.getX(), invokerLocation.getY(), invokerLocation.getZ());
 
-                    Vector knockbackVector = new Vector(
+                    final Vector knockbackVector = new Vector(
                             x,
                             z,
                             y
                     );
 
-                    float scalar = 0.5f;
+                    HashSet<LivingEntity> nearbyEntities = new HashSet<>();
+                    for (Entity entity : world.getNearbyEntities(invokerLocation, radius, radius, radius)) {
+                        if (entity instanceof LivingEntity && entity != invoker) {
+                            nearbyEntities.add((LivingEntity) entity);
+                        }
+                    }
 
                     HashSet<LivingEntity> protectedEntities = new HashSet<>();
-
                     protectedEntities.add(invoker.getPlayer());
 
-                    world.playSound(playerLoc, Sound.BLOCK_SHROOMLIGHT_PLACE, 2, 2);
+                    world.playSound(invokerLocation, Sound.BLOCK_SHROOMLIGHT_PLACE, 2, 2);
 
-                    for (int count = 0; count < maxIterations; count++) {
+                    Location particleSpawnLoc = invokerLocation;
+
+                    int count = 0;
+                    for (; count < maxIterations; count++) {
                         scalar += scalarIncrement;
 
                         invokerLocationVector = invokerLocationVector.add(invokerDirectionFacingVector.scalarMultiply(scalar));
@@ -82,7 +93,12 @@ public class WandFunctionality implements Listener {
                                 invokerLocationVector.getZ()
                         );
 
-                        Location particleSpawnLoc = particleSpawnVector.toLocation(world);
+                        particleSpawnLoc = particleSpawnVector.toLocation(world);
+
+                        if (particleSpawnLoc.distanceSquared(blockLookingAt) < 1.5) {
+                            broken = true;
+                            break;
+                        }
 
                         world.spawnParticle(Particle.FLAME, particleSpawnLoc, 1, 0, 0, 0, 0.02, null, true);
 
@@ -93,25 +109,29 @@ public class WandFunctionality implements Listener {
                         if (Utils.getRandomBoolean(0.2f)) {
                             world.spawnParticle(Particle.CAMPFIRE_COSY_SMOKE, particleSpawnLoc, 0, vX, vY, vZ, 10, null, true);
                         }
+                        getServer().getConsoleSender().sendMessage("ARE YOU HERE?");
+                        for (LivingEntity entity : nearbyEntities) {
+                            if (particleSpawnLoc.distanceSquared(entity.getLocation()) < 1 && !protectedEntities.contains(entity)) {
+                                entity.damage(damage);
+                                entity.setNoDamageTicks(0);
+                                getServer().getConsoleSender().sendMessage("OR HERE?");
+                                entity.setVelocity(knockbackVector.multiply(1.5));
 
-                        for (Entity entity : world.getNearbyEntities(particleSpawnLoc, 1,1,1)) {
-                            if (entity instanceof LivingEntity && !protectedEntities.contains(entity)) {
-                                LivingEntity livingEntity = (LivingEntity) entity;
-                                livingEntity.damage(damage);
-                                livingEntity.setNoDamageTicks(0);
-
-                                livingEntity.setVelocity(knockbackVector.multiply(1.5));
-
-                                Location loc = livingEntity.getLocation();
+                                Location loc = entity.getLocation();
                                 world.playSound(loc, Sound.ENTITY_FIREWORK_ROCKET_BLAST, 1.2f, 2);
 
-                                protectedEntities.add(livingEntity);
+                                protectedEntities.add(entity);
                             }
                         }
-
-
-
                     }
+
+                    if (broken) {
+                        world.createExplosion(particleSpawnLoc, (maxIterations - count) * 0.10f);
+                        world.spawnParticle(Particle.FIREWORKS_SPARK, particleSpawnLoc, 32, 0.2, 0.2, 0.2, 0.5);
+                    }
+
+                    long now = System.currentTimeMillis();
+                    getServer().getConsoleSender().sendMessage(""+ (now-then));
                 }
             }
         }
